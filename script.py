59156856f023b013f6fd73fb3d65e85ec10036e2
EXTERNAL Anton Borisenkov (EPAM Systems, ETAS.NET) <Anton_Borisenkov@epam.com>
2020-09-29 19:00:01 +0400

    working on py-script

--- script.py
+++ script.py
@@ -1,5 +1,14 @@
+#!/usr/bin/env python3.8
+
+import argparse
+import enum
+import itertools
+import logging
 import os
+import sys
+import pathlib
 
 print ('this is python script')
-var1 = os.environ.get('PASSWORD')
+passwd = os.environ.get('PASSWORD')
+var1 = "sonar.password={}".format(passwd)
 print(var1)
