32aa1a6aaa2434c3c730da109e96de0592afbb62
tanon-adm <tanonbox@gmail.com>
2021-10-28 15:49:25 +0400

    Update core.groovy
    failbuild() added

--- core.groovy
+++ core.groovy
@@ -1,5 +1,15 @@
+def firstFail = true
+
 def Test() {
     //marker = true
+}
+
+def failBuild() {
+    currentBuild.result = 'FAILURE'
+    if (firstFail) {
+        currentBuild.description = "Failed in stage \"${STAGE_NAME}\""
+        firstFail = false
+    }
 }
 
 // Do not remove this line. It is used for importing the file into the Jenkinsfile.
